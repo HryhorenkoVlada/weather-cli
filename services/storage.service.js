c6e5b2606452a9093ad938269c17942c167d239f
HryhorenkoVlada <vlada.hryhorenko@gotoinc.co>
2023-05-16 01:45:55 +0300

    feat: add service to save and read data form file

---
+++ services/storage.service.js
@@ -0,0 +1,34 @@
+import { homedir } from 'os';
+import { join, basename } from 'path';
+import * as fs from 'fs/promises';
+
+const filePath = join(homedir(), 'weather-data.json');
+
+const isExist = async (path) => {
+  try {
+    await fs.stat(path);
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
+
+export const saveKeyValue = async (key, value) => {
+  let data = {};
+  if (await isExist(filePath)) {
+    const file = await fs.readFile(filePath);
+    data = JSON.parse(file);
+  }
+  data[key] = value;
+
+  await fs.writeFile(filePath, JSON.stringify(data));
+};
+
+export const getKeyValue = async (key) => {
+  if (await isExist(filePath)) {
+    const file = await fs.readFile(filePath);
+    const data = JSON.parse(file);
+    return data[key];
+  }
+  return null;
+};
